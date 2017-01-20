014fc451540b82d047a12e938512178bbbd9f295
SunZilla <laurin@sunzilla.de>
2017-01-20 17:48:52 +0100

    Add files via upload

---
+++ 3 - SOFWARE/ESP8266/incoming_serial.ino
@@ -0,0 +1,151 @@
+String serial_msg_ok;
+String serial_msg;
+String hist_data;
+int begin_msg;
+int end_msg;  
+  void send_command_to_arduino(String command){
+    Serial << F("score ") << command << "\n";    
+  }
+  
+boolean wait_for_answer_and_timeout(int command){
+      int begin_time = millis();  
+      int got_answer = 0;
+        while (got_answer == 0) {
+          Serial.print(".");
+           int incoming_command = handle_Serial_Command();  
+           //Serial << incoming_command;
+           if(incoming_command == command) { return true; break; got_answer = 1;}
+           delay(100);
+          if((begin_time+timeout)<millis()){
+            wifi_mode = 1;
+            Serial.println("No answer recevied.");
+            break;
+            return false;
+          }
+      }   
+}
+int handle_Serial_Command(){
+        if ( Serial.available()) {
+        
+        serial_msg = Serial.readStringUntil('\n');
+        begin_msg = serial_msg.lastIndexOf('#');
+        end_msg = serial_msg.lastIndexOf('%');
+        if(end_msg != -1 && begin_msg != -1){
+          serial_msg_ok = serial_msg.substring(begin_msg+1,end_msg-1);   
+          int received_command = seperate_string(serial_msg_ok); 
+          
+          
+          return received_command;          
+        }
+      }
+      
+}
+
+int seperate_string(String str){
+  int command = str.substring(0,str.indexOf(';')).toInt();
+
+  str = str.substring(str.indexOf(';')+1);
+  if(command == 1){  // Read Genearl status Message
+    live_data = str;
+    uptime = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    V_Bus = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+  
+    SOC = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    E_Bat = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    
+    E_PV = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    state_PV = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    E_AC = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    state_AC = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    E_DC = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    state_DC = str.substring(0,str.indexOf(';'));
+    str = str.substring(str.indexOf(';')+1);
+    
+    event = str.substring(0,str.indexOf(';'));
+             if(last_sent_to_server + 30000 < millis())
+            {
+              send_to_server(live_data);
+              last_sent_to_server = millis();
+            }
+            calculate_powers();
+  }
+  else if (command == 2){
+        hist_data = hist_data+str;
+        Serial.println("histrocial data collected");
+  }
+  else if (command == 3){
+        filename = str;
+  }  
+else if (command == 50){ // Got Wifi credentials
+        ssid_client = str.substring(0,str.indexOf(';'));
+         str = str.substring(str.indexOf(';')+1);
+        pwd_client = str.substring(0,str.indexOf(';'));        
+  }
+
+  else if (command == 52){ 
+          //Serial.println(str);
+          AP_NameString = str.substring(0,str.indexOf(';'));
+        
+          str = str.substring(str.indexOf(';')+1);
+          AP_pwdString = str.substring(0,str.indexOf(';'));   
+
+  }  
+  else if (command == 54){
+        
+        wifi_mode = str.substring(0,str.indexOf(';')).toInt();
+
+  }
+
+  else if (command == 56){
+        String user = str.substring(0,str.indexOf(';'));
+      
+        str = str.substring(str.indexOf(';')+1);
+        String pwd = str.substring(0,str.indexOf(';'));  
+        Serial << user << "\n"<< pwd << "\n";
+        str = str.substring(str.indexOf(';')+1);
+        permission = str.substring(0,str.indexOf(';')).toInt(); 
+        
+        www_username[user.length() + 1];
+        memset(www_username, 0, user.length() + 1);
+      
+        for (int i=0; i<user.length(); i++)
+          www_username[i] = user.charAt(i);
+    
+        
+        www_password[user.length() + 1];
+        memset(www_password, 0, pwd.length() + 1);
+      
+        for (int i=0; i<pwd.length(); i++)
+          www_password[i] = pwd.charAt(i);      
+
+   }
+  else if (command == 60){
+        
+        initial_config = str.substring(0,str.indexOf(';')).toInt();
+
+  }   
+  else if (command == 68){
+        
+        ssl_cert = str.substring(0,str.indexOf(';'));
+        //Serial << "\n yes cert " << ssl_cert << "\n";
+
+  }  
+   return command;
+}
---
+++ 3 - SOFWARE/ESP8266/wifi_and_permission.ino
@@ -0,0 +1,83 @@
+
+
+void get_start_wifi(){
+        send_command_to_arduino(String(54));
+        wait_for_answer_and_timeout(54); 
+      
+        if(wifi_mode == 1)      { WiFi.mode(WIFI_AP);     setup_own_wifi(); }
+        else if(wifi_mode == 2) { WiFi.mode(WIFI_AP_STA); setup_own_wifi(); setup_wifi_client(); }
+        else if(wifi_mode == 3) { WiFi.mode(WIFI_STA);                      setup_wifi_client(); }
+  }
+  
+ void setup_own_wifi(){
+    send_command_to_arduino(String(52));
+    Serial.println("Setting up WiFi ");
+    if(!wait_for_answer_and_timeout(52)){
+      Serial << " Set up with default credentials";
+    }
+    else {
+      Serial << "received creds";
+    }
+      //String AP_NameString = "SunZilla Energy " + identity;
+      AP_NameString = AP_NameString +"_" + identity;
+      
+      char AP_NameChar[AP_NameString.length() + 1];
+      memset(AP_NameChar, 0, AP_NameString.length() + 1);
+    
+      for (int i=0; i<AP_NameString.length(); i++)
+        AP_NameChar[i] = AP_NameString.charAt(i);
+  
+            char AP_pwdChar[AP_pwdString.length() + 1];
+      memset(AP_pwdChar, 0, AP_pwdString.length() + 1);
+    
+      for (int i=0; i<AP_pwdString.length(); i++)
+        AP_pwdChar[i] = AP_pwdString.charAt(i);   
+
+    WiFi.softAP(AP_NameChar, AP_pwdChar);
+    Serial << AP_NameChar << "\n" << AP_pwdChar << "\n";
+        Serial.print("Logon 192.168.4.1");
+  }
+void setup_wifi_client(){
+  
+  send_command_to_arduino("50");
+  if(!wait_for_answer_and_timeout(50)){
+    // Set up with default credentials
+  }
+  else{
+    Serial << "received. trying to connect";
+      
+  }
+  char char_ssid_client[ssid_client.length() + 1];
+  memset(char_ssid_client, 0, ssid_client.length() + 1);
+  for (int i=0; i<ssid_client.length(); i++)  char_ssid_client[i] = ssid_client.charAt(i);
+
+  char char_pwd_client[pwd_client.length() + 1]; 
+  memset(char_pwd_client, 0, pwd_client.length() + 1);
+  for (int i=0; i<pwd_client.length(); i++)  char_pwd_client[i] = pwd_client.charAt(i);  
+    WiFi.begin(char_ssid_client, char_pwd_client);
+    WiFi.begin(char_ssid_client, char_pwd_client);
+    WiFi.begin(char_ssid_client, char_pwd_client);
+
+    int wifi_connect_begin = millis();
+
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(200);
+        if((wifi_connect_begin+timeout)<millis()){
+          
+          Serial.print("No connection to " + ssid_client +". Setting up own Wifi.");
+          setup_own_wifi();
+          break;
+        }
+    } 
+}
+
+
+void convert_IP(){
+        IPAddress localAddr = WiFi.localIP();
+        byte oct1 = localAddr[0];
+        byte oct2 = localAddr[1];
+        byte oct3 = localAddr[2];
+        byte oct4 = localAddr[3];
+        sprintf(IP, "%d.%d.%d.%d", oct1, oct2, oct3, oct4);
+}
